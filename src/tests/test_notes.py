77948ef110af239650e0ea09505a6981f9e2b3c5
Daniel Santos <drsantos20@gmail.com>
2022-03-15 00:48:08 -0300

    Feat add validation (#8)
    * feat: add delete route
    
    * feat: add pydantic validation

--- src/tests/test_notes.py
+++ src/tests/test_notes.py
@@ -5,6 +5,27 @@ import pytest
 from app.api import crud
 
 
+@pytest.mark.parametrize(
+    "id, payload, status_code",
+    [;lm
+        [1, {}, 422],
+        [1, {"description": "bar"}, 422],
+        [999, {"title": "foo", "description": "bar"}, 404],
+        [1, {"title": "1", "description": "bar"}, 422],
+        [1, {"title": "foo", "description": "1"}, 422],
+        [0, {"title": "foo", "description": "bar"}, 422],
+    ],
+)
+def test_update_note_invalid(test_app, monkeypatch, id, payload, status_code):
+    async def mock_get(id):
+        return None
+
+    monkeypatch.setattr(crud, "get", mock_get)
+
+    response = test_app.put(f"/notes/{id}/", data=json.dumps(payload),)
+    assert response.status_code == status_code
+
+
 def test_create_note(test_app, monkeypatch):
     test_request_payload = {"title": "something", "description": "something else"}
     test_response_payload = {"id": 1, "title": "something", "description": "something else"}
@@ -142,3 +163,26 @@ def test_read_note_incorrect_id(test_app, monkeypatch):
 
     response = test_app.get("/notes/0")
     assert response.status_code == 422
+    
+
+def test_create_note_invalid_json(test_app):
+    response = test_app.post("/notes/", data=json.dumps({"title": "something"}))
+    assert response.status_code == 422
+
+    response = test_app.post("/notes/", data=json.dumps({"title": "1", "description": "2"}))
+    assert response.status_code == 422
+
+
+def test_remove_note_incorrect_id(test_app, monkeypatch):
+    async def mock_get(id):
+        return None
+
+    monkeypatch.setattr(crud, "get", mock_get)
+
+    response = test_app.delete("/notes/999/")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Note not found"
+
+    response = test_app.delete("/notes/0/")
+    assert response.status_code == 422
+    
